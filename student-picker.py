1b78184206a13d325526ecedc24543eb37e43276
Kedar Iyer <kedarmail@gmail.com>
2015-12-09 09:12:05 -0800

    remove Eric and Charlie

--- student-picker.py
+++ student-picker.py
@@ -1,5 +1,5 @@
 import random
 
-students = [ 'Fran', 'Thomas', 'Marcus', 'Michelle', 'Eric', 'Andrea', 'GaMaur', 'John Kwak', 'Nick', 'La', 'Edward', 'Jack', 'Michael', 'Adam', 'Charlie', 'Morgan', 'Stacey', 'Chad', 'John Hwang']
+students = [ 'Fran', 'Thomas', 'Marcus', 'Michelle', 'Andrea', 'GaMaur', 'John Kwak', 'Nick', 'La', 'Edward', 'Jack', 'Michael', 'Adam', 'Morgan', 'Stacey', 'Chad', 'John Hwang']
 
 print(students[random.randrange(len(students))])
