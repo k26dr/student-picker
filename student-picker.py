03434558c4ad7c90999cff01d4fe223d6e34835b
Kedar Iyer <kedarmail@gmail.com>
2016-03-14 15:40:44 -0700

    picker groups

--- student-picker.py
+++ student-picker.py
@@ -1,5 +1,22 @@
 import random
+import sys
 
-students = [ 'Fran', 'Thomas', 'Marcus', 'Michelle', 'Andrea', 'GaMaur', 'John Kwak', 'Nick', 'La', 'Edward', 'Jack', 'Michael', 'Adam', 'Morgan', 'Stacey', 'Chad', 'John Hwang']
+students = ["Carlos A.", "Noah", "Angie", "Arvin", "Victor", "Andrew", "Timmy", "Martin", "AJ", "Brigette", "Marcos", "Jonathan", "Jobeth", "Max", "Carlos B.", "Trevor", "Joseph", "Jeffrey"]
 
-print(students[random.randrange(len(students))])
+# Groups
+if len(sys.argv) > 1 and sys.argv[1] == 'groups':
+    random.shuffle(students)
+    print("Group 1 (Javascript Jedis)")
+    print(students[0:4])
+
+    print("\nGroup 2 (Carlton's Callbacks)")
+    print(students[4:8])
+
+    print("\nGroup 3 (Infinite Loop)")
+    print(students[8:13])
+
+    print("\nGroup 4 (Beliebers)")
+    print(students[13:])
+    
+else:
+    print(students[random.randrange(len(students))])
